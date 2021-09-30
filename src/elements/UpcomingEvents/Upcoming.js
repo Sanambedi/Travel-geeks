ee4717a60bb42f543230ab7923e42aca9bfd19d3
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-09-30 23:56:05 +0530

    origin

--- src/elements/UpcomingEvents/Upcoming.js
+++ src/elements/UpcomingEvents/Upcoming.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import "./Upcoming.css";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 export default function Upcoming(){
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     return(
         <div>
             <div className="bg-ground">
                 <p>Checkout best promotional tour</p>
-                <h1>Upcoming Events</h1>
+                <h1>Upcoming Events</h1>    
             </div>
+            <div className="carousel">
+                <Carousel 
+                responsive={responsive} >
+                    <div><img src="https://picsum.photos/200/300" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/300" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/300" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/300" alt=""/></div>
+                </Carousel>
+            </div>
+                
+            
         </div>
     )
 }
