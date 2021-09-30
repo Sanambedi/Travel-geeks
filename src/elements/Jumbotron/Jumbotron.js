d96111baa3abf852c32dae5995719a996ca5372c
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-09-30 16:56:41 +0530

    origin

--- src/elements/Jumbotron/Jumbotron.js
+++ src/elements/Jumbotron/Jumbotron.js
@@ -9,26 +9,7 @@ export default function Jumbotron(){
                     <p className="">Let's start your journey with us , your dream will come true</p>
                 </div>
                 <div className="button">Explore Destinations<span></span><span></span><span></span><span></span></div>
-                <nav>
-  <ul>
-    <li>
-      home
-      <span></span><span></span><span></span><span></span>
-    </li>
-    <li>
-      products
-      <span></span><span></span><span></span><span></span>
-    </li>
-    <li>
-      services
-      <span></span><span></span><span></span><span></span>
-    </li>
-    <li>
-      contact
-      <span></span><span></span><span></span><span></span>
-    </li>
-  </ul>
-</nav>
+                
             </div>
         </div>
     )
