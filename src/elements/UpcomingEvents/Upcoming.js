f15fe4efbaf4238af252fc97f34b47605e913c92
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-11-14 17:28:04 +0530

    origin

--- src/elements/UpcomingEvents/Upcoming.js
+++ src/elements/UpcomingEvents/Upcoming.js
@@ -26,16 +26,17 @@ export default function Upcoming(){
       };
     return(
         <div>
-            <div className="bg-ground">
-                <p>Checkout best promotional tour</p>
-                <h1>Upcoming Events</h1>    
+          <div className="back-back">
+            <div className="bg-ground1">
+                <p className="checkout-paragraph">Checkout Our Best Promotional Tours</p>
+                <h1 className="checkout-heading">Upcoming Events</h1>    
             </div>
+          </div>
             <div className="carousel">
                 <Carousel
                 infinite={true} 
                 keyBoardControl={true}
                 transitionDuration={500}
-                
                 responsive={responsive} >
                     <div><Card cardImage="https://www.collinsdictionary.com/images/full/lake_281848937_1000.jpg"/></div>
                     <div><Card cardImage="https://www.collinsdictionary.com/images/full/lake_281848937_1000.jpg"/></div>
