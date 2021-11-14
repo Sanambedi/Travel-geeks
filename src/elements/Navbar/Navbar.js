4b8091187b1e46a103ef6e58fdb460e98a23a3c6
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-11-14 16:41:09 +0530

    origin

--- src/elements/Navbar/Navbar.js
+++ src/elements/Navbar/Navbar.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./Navbar.css";
+
 import logo from "../../resources/newLogo-removebg-preview.png";
 import { MdCall } from 'react-icons/md';
 import { FcMenu } from 'react-icons/fc';
@@ -7,33 +8,48 @@ import { Link } from 'react-router-dom';
 
 
 export default function Navbar(){
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="mainest-nav">
-            <div className="main-nav">
-                <div className="image-container">
-                    <img src={logo} alt="" className="top-nav-logo"/>
-                </div>
-                <div className="headings">
-                    <ul className="nav-headings">
-                        <li>
-                            <Link className="nav-link" to="/">Home</Link>    
-                        </li>
-                        <li><Link to="/Events" className="nav-link">Events</Link></li>
-                        <li><Link to="/About" className="nav-link">About</Link></li>
-                        <li><Link to="/Blog" className="nav-link">Blog</Link></li>
-                        <li><Link to="/Contact" className="nav-link">Contact</Link></li>   
-                        <li className="call-button">
-                            <a href="tel:+917015024295" className="phoneLink"><MdCall className="callIcon"/> +91 70150 24295</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="mobile-menu">
-                    <FcMenu className="menu-button"/>
+        <div>
+            <div className="mainest-nav">
+                <div className="main-nav">
+                    <div className="image-container">
+                        <img src={logo} alt="" className="top-nav-logo"/>
+                    </div>
+                    <div className="headings">
+                        <ul className="nav-headings">
+                            <li>
+                                <Link className="nav-link" to="/">Home</Link>    
+                            </li>
+                            <li><Link to="/Events" className="nav-link">Events</Link></li>
+                            <li><Link to="/About" className="nav-link">About</Link></li>
+                            <li><Link to="/Blog" className="nav-link">Blog</Link></li>
+                            <li><Link to="/Contact" className="nav-link">Contact</Link></li>   
+                            <li className="call-button">
+                                <a href="tel:+917015024295" className="phoneLink"><MdCall className="callIcon"/> +91 70150 24295</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="mobile-menu-button" onClick={()=>{
+                        setIsOpen(!isOpen)
+                    }}>
+                        <FcMenu className="menu-button"/>
+                    </div>
                 </div>
             </div>
-            <div className="mobile-menu">
-                <div className="bottom-nav">Hello</div>
+            <div className={isOpen ? "hidden" : "mobile-menu"}>
+                <div className="bottom-nav">Home</div>
+                <div className="bottom-nav">Events</div>
+                <div className="bottom-nav">About</div>
+                <div className="bottom-nav">Blog</div>
+                <div className="bottom-nav">Contact</div>
+                <div className="bottom-nav">
+                    <div className="call-button2">
+                        <a href="tel:+917015024295" className="phoneLink"><MdCall className="callIcon"/> +91 70150 24295</a>
+                    </div>
+                </div>
             </div>
         </div>
+        
     )
 }
